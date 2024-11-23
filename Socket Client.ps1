8f9f3a662a547265ade2e3fef0291964318bc138
Max <blue.shark@disroot.org>
2024-11-23 18:49:54 +0300

    Update Socket Client.ps1

--- Socket Client.ps1
+++ Socket Client.ps1
@@ -1,4 +1,54 @@
-$s=@'
-function Send-SocketCommand{[CmdletBinding()]param([Parameter(Mandatory=$true)][string]$h,[Parameter(Mandatory=$true)][string]$c,[Parameter(Mandatory=$false)][string]$d,[Parameter(Mandatory=$false)][string]$s);try{$p=9028;$t=New-Object Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork,[Net.Sockets.SocketType]::Stream,[Net.Sockets.ProtocolType]::Tcp);$t.Connect($h,$p);$r=@{Command=$c;Data=$d;Second=$s}|ConvertTo-Json;$b=[Text.Encoding]::UTF8.GetBytes($r);$t.Send($b)|Out-Null;$f=New-Object byte[] 1048576;$v=$t.Receive($f);$e=[Text.Encoding]::UTF8.GetString($f,0,$v);Write-Output $e}catch{Write-Error "Error: $_"}finally{if($t){$t.Close()}}}Set-Alias sc Send-SocketCommand
-'@
-iex $s
+function Send-SocketCommand {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ServerIp,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$Command,
+        
+        [Parameter(Mandatory=$false)]
+        [string]$Data,
+        
+        [Parameter(Mandatory=$false)]
+        [string]$Second
+    )
+
+    $port = 9028
+    
+    try {
+        $socket = New-Object System.Net.Sockets.Socket(
+            [System.Net.Sockets.AddressFamily]::InterNetwork,
+            [System.Net.Sockets.SocketType]::Stream,
+            [System.Net.Sockets.ProtocolType]::Tcp
+        )
+
+        $socket.Connect($ServerIp, $port)
+
+        $requestData = @{
+            Command = $Command
+            Data = $Data
+            Second = $Second
+        } | ConvertTo-Json
+
+        $requestBytes = [System.Text.Encoding]::UTF8.GetBytes($requestData)
+        $socket.Send($requestBytes) | Out-Null
+
+        $buffer = New-Object byte[] 1048576
+        $received = $socket.Receive($buffer)
+        
+        $response = [System.Text.Encoding]::UTF8.GetString($buffer, 0, $received)
+        Write-Output $response
+    }
+    catch {
+        Write-Error "Ошибка подключения: $_"
+    }
+    finally {
+        if ($socket) {
+            $socket.Close()
+        }
+    }
+}
+
+# Пример использования
+# Send-SocketCommand -ServerIp "192.168.1.100" -Command "echo" -Data "Привет" -Second "Мир"
